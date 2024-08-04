30231cde7c4d9b5aa3858a2b6674dacfd1267a7d
Shehzadz4f <shehzad.z4f@gmail.com>
2024-08-04 22:53:40 +0530

    Added css style and layout like iphone calculator and visually interactive operation buttons.
    Also fixed a bug where performing subtraction from zero was resulting in a positive result.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let num2 = '';
 let operatorSelected = '';
 let previousButton;
 let plusMinus = false;
+let operatorButtonEngaged = false;
+let previousElement;
 
 function operate(n1, n2, op) {
   switch(op) {
@@ -49,43 +51,58 @@ const operatorButtons = document.querySelectorAll('.js-operator-buttons');
 const resetButton = document.querySelector('.js-reset-button');
 const equalsToButton = document.querySelector('.js-equals-to-button');
 const plusMinusButton = document.querySelector('.js-plus-minus-button');
+const percentageButton = document.querySelector('.js-percentage-button');
+
 
 numberButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
-   getDisplayNumber(event);
+    if (operatorButtonEngaged) {
+    operatorButtonEngaged = false;
+    previousElement.classList.remove('operator-button-engaged');
+    }
+    getDisplayNumber(event);
   });
 });
 
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      if (display.textContent == 0 && button.textContent === '-') {
-        display.textContent = '-';
-        plusMinus = true;
-      } else if (num1 === '') {
-        num1 = Number(display.textContent);
-        console.log('num1', num1);
-  
-      } else if(num1 !== '') {
-        num2 = Number(display.textContent);
-        console.log('num2', num2);
-      }
+  button.addEventListener('click', (event) => {
+    if (!operatorButtonEngaged) {
+      operatorButtonEngaged = true;
+      button.classList.add('operator-button-engaged');
+    } else if (isNaN(Number(previousButton))) {
+      console.log('else if triggered');
+      previousElement.classList.remove('operator-button-engaged');
+      button.classList.add('operator-button-engaged');
+    }
 
-      if (num2 !== '') {
-        if (Number(previousButton)) {
-          num1 = operate(num1, num2, operatorSelected);
-          display.textContent = num1;
-          console.log('new num1', num1);
-        }
-        
-      }
-      operatorSelected = button.textContent;
-      if (operatorSelected === '%') {
+    if (display.textContent == 0 && button.textContent === '-') {
+      plusMinus = true;
+    } else if (num1 === '') {
+      num1 = Number(display.textContent);
+      console.log('num1', num1);
+
+    } else if(num1 !== '') {
+      num2 = Number(display.textContent);
+      console.log('num2', num2);
+    }
+
+    if (num2 !== '') {
+      if (Number(previousButton)) {
         num1 = operate(num1, num2, operatorSelected);
         display.textContent = num1;
+        console.log('new num1', num1);
       }
-      console.log('operatorSelected',operatorSelected);
-      previousButton = button.textContent;    
-      console.log (previousButton);
+      
+    }
+    operatorSelected = button.textContent;
+    if (operatorSelected === '%') {
+      num1 = operate(num1, num2, operatorSelected);
+      display.textContent = num1;
+    }
+    console.log('operatorSelected',operatorSelected);
+    previousButton = button.textContent;    
+    console.log (previousButton);
+    previousElement = event.target;
     });
   });
 
@@ -105,11 +122,23 @@ plusMinusButton.addEventListener('click', () => {
 });
 
 resetButton.addEventListener('click', () => {
+  if (operatorButtonEngaged) {
+    operatorButtonEngaged = false;
+    previousElement.classList.remove('operator-button-engaged');
+  }
   display.textContent = 0;
   previousButton = undefined;
   num1 = '';
   num2 = '';
   operatorSelected = '';
+});
+
+percentageButton.addEventListener('click', () => {
+  console.log('event triggered');
+  num1 = Number(display.textContent);
+  operatorSelected = percentageButton.textContent;
+  num1 = operate(num1, num2, operatorSelected);
+  display.textContent = num1;
 });
 
 equalsToButton.addEventListener('click', () => {
