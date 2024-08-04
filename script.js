c8408e5fd6cfdfbd1e26e45a1851162ff49874ab
Shehzadz4f <shehzad.z4f@gmail.com>
2024-08-04 09:00:51 +0530

    Added functionality to the plus/minus and percent button.
    Changed class name and added click eventlistener to the percent button where it divides any number by 100.
    Modified class name for plus minus button and added click eventlistener to plus minus button.
    Toggling plus minus button changes the display number from  positive to negative and vice versa.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let num1 = '';
 let num2 = '';
 let operatorSelected = '';
 let previousButton;
+let plusMinus = false;
 
 function operate(n1, n2, op) {
   switch(op) {
@@ -47,6 +48,7 @@ const numberButtons = document.querySelectorAll('.js-number-buttons');
 const operatorButtons = document.querySelectorAll('.js-operator-buttons');
 const resetButton = document.querySelector('.js-reset-button');
 const equalsToButton = document.querySelector('.js-equals-to-button');
+const plusMinusButton = document.querySelector('.js-plus-minus-button');
 
 numberButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
@@ -58,6 +60,7 @@ operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
       if (display.textContent == 0 && button.textContent === '-') {
         display.textContent = '-';
+        plusMinus = true;
       } else if (num1 === '') {
         num1 = Number(display.textContent);
         console.log('num1', num1);
@@ -66,16 +69,35 @@ operatorButtons.forEach((button) => {
         num2 = Number(display.textContent);
         console.log('num2', num2);
       }
-      if (num2 !== '' ) {
+      if (num2 !== '') {
         num1 = operate(num1, num2, operatorSelected);
         display.textContent = num1;
         console.log('new num1', num1);
       }
       operatorSelected = button.textContent;
+      if (operatorSelected === '%') {
+        num1 = operate(num1, num2, operatorSelected);
+        display.textContent = num1;
+      }
       console.log('operatorSelected',operatorSelected);
       previousButton = button.textContent;    
     });
   });
+
+plusMinusButton.addEventListener('click', () => {
+  if (plusMinus === false) {
+    display.textContent = '-' + display.textContent;
+    plusMinus = true;
+  } 
+  else if (plusMinus === true) {
+    display.textContent = display.textContent
+      .split('')
+      .toSpliced(0,1)
+      .join('');
+    console.log (display.textContent)
+    plusMinus = false; 
+  }
+});
 
 resetButton.addEventListener('click', () => {
   display.textContent = 0;
